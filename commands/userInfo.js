22d79f1b35ff0836ca641ea8313b969d0d423e7c
Curzeed <87865741+Curzeed@users.noreply.github.com>
2022-04-09 19:08:38 +0200

    Update userInfo.js

--- commands/userInfo.js
+++ commands/userInfo.js
@@ -1,23 +1,15 @@
 const {SlashCommandBuilder} = require('@discordjs/builders');
 const { Client, Intents, Collection } = require('discord.js');
 const {MessageEmbed} = require("discord.js");
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('info')
         .setDescription('Replies with user infos'),
-
+    
     async execute(interaction) {
         var userDs;
-        
         const channel = interaction.channel;
-        const tabCompliment = ["T'es si beau que j'te sucerai juste pour le goût","T'es si beau que quand je te vois, ma gorge s'élargit","Parfois aux toilettes, je pense à toi, et je bande quand je fais caca",
-        "Je traverserai la Méditerranée à la nage pour t'offrir un esclave", "T'es beau comme un coup droit de Roger Federer", "Tu me fais autant rêver qu'un menu Golden",
-        "J'aimerais être la chaise sur laquelle tu t'assois", "T'es beaucoup moins con que t'en as l'air"];
-        let randomNum = getRandomInt(tabCompliment.length);
         await interaction.user.fetch(true).then(function(data) {
             var userid = data;
             console.log(userid);
@@ -31,7 +23,6 @@ module.exports = {
         embedMes.setThumbnail(userDs.avatarURL());
         embedMes.setTitle(userDs.username);
         embedMes.setColor(userDs.hexAccentColor);
-        embedMes.setDescription(tabCompliment[randomNum]);
         embedMes.setTimestamp();
         embedMes.addField('**Compte crée le : **\n',`${dateAcc}`);
         
